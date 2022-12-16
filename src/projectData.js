7a8f5c9d1e17dcfccc865d9fac2f84fb509bb641
jacer <jacerdjo@gmail.com>
2022-12-16 09:55:56 +0100

    adding the shoppi MERN project

--- src/projectData.js
+++ src/projectData.js
@@ -1,100 +1,182 @@
 const projects = [
-    {
-        name: "Memory Game",
-        imageUrl: "./images/memory-game.jpg",
-        link: `https://memory-game.jacerchetoui.me`,
-        keywords: ["useState", "useEffect", "localStorage", "Events", "conditional rendering"]
-    },
-    {
-        name: "ToDo App",
-        imageUrl: "./images/todo.jpg",
-        link: `https://todoapp.jacerchetoui.me`,
-        keywords: ["useState", "useEffect", "useRef", "form", "localStorage", "Events", "conditional rendering"]
-    },
-    {
-        name: "Tenzies Game",
-        imageUrl: "./images/tenzies-game.jpg",
-        link: `https://tenzies-game.jacerchetoui.me`,
-        keywords: ["useState", "useEffect", "Events", "conditional rendering"]
-    },
-    {
-        name: "GitFinder",
-        imageUrl: "./images/gitfinder.jpg",
-        link: `https://githubfinder.jacerchetoui.me`,
-        keywords: ["tailwindCSS", "useState", "useEffect", "Events", "Fetch API"]
-    },
-    {
-        name: "Quizzical",
-        imageUrl: "./images/quizzical.jpg",
-        link: `https://quizzical.jacerchetoui.me`,
-        keywords: ["useState", "useEffect", "useMemo", "Events", "conditional rendering", "fetch API"]
-    },
-    {
-        name: "Shopping Cart",
-        imageUrl: "./images/cart.jpg",
-        link: `https://shopping-cart.jacerchetoui.me/`,
-        keywords: ["SCSS", "useReducer", "useContext", "useState", "useEffect", "Events", "conditional rendering"]
-    },
-    {
-        name: "Stripe Submenu",
-        imageUrl: "./images/stripe.jpg",
-        link: `https://stripe-submenu.jacerchetoui.me`,
-        keywords: ["useContext", "useRef", "useState", "Events", "conditional rendering"]
-    },
-    {
-        name: "SideBar",
-        imageUrl: "./images/sidebar.jpg",
-        link: `https://sidebar.jacerchetoui.me`,
-        keywords: ["useContext", "custom Hooks", "useState", "Events", "conditional rendering"]
-    },
-    {
-        name: "Meme Generator",
-        imageUrl: "./images/memegenerator.jpg",
-        link: `https://memegenerator.jacerchetoui.me`,
-        keywords: ["useState", "useEffect", "form", "Events", "conditional rendering", "fetch API"]
-    },
-    {
-        name: "Slider",
-        imageUrl: "./images/slider.jpg",
-        link: `https://slider.jacerchetoui.me`,
-        keywords: ["useState", "useEffect", "Events", "conditional rendering"]
-    },
-    {
-        name: "Tours",
-        imageUrl: "./images/tours.jpg",
-        link: `https://tours.jacerchetoui.me`,
-        keywords: ["useState", "useEffect", "fetch API", "Events", "conditional rendering"]
-    },
-    {
-        name: "Color Generator",
-        imageUrl: "./images/color-generator.jpg",
-        link: `https://colorgenerator.jacerchetoui.me`,
-        keywords: ["form", "useState", "useEffect", "fetch API", "Events", "conditional rendering"]
-    },
-    {
-        name: "Reviews",
-        imageUrl: "./images/reviews.jpg",
-        link: `https://reviews.jacerchetoui.me`,
-        keywords: ["useState", "Events"]
-    },
-    {
-        name: "navbar",
-        imageUrl: "./images/navbar.jpg",
-        link: `https://navbar.jacerchetoui.me`,
-        keywords: ["useState", "useEffect", "useRef", "Events", "conditional rendering"]
-    },
-    {
-        name: "Food Menu",
-        imageUrl: "./images/food-menu.jpg",
-        link: `https://foodmenu.jacerchetoui.me`,
-        keywords: ["useState", "Events", "conditional rendering"]
-    },
-    {
-        name: "Lorem Generator",
-        imageUrl: "./images/lorem-generator.jpg",
-        link: `https://loremgenerator.jacerchetoui.me`,
-        keywords: ["useState", "conditional rendering", "form"]
-    },
-]
+  {
+    name: "Shoppi",
+    imageUrl: "./images/shoppi.png",
+    link: `https://shoppi.jacerchetoui.me`,
+    keywords: [
+      "Context API",
+      "react-router",
+      "Nodejs",
+      "express",
+      "mongoDB",
+      "Authentication",
+      "axios",
+      "stripe",
+    ],
+  },
+  {
+    name: "Memory Game",
+    imageUrl: "./images/memory-game.jpg",
+    link: `https://memory-game.jacerchetoui.me`,
+    keywords: [
+      "useState",
+      "useEffect",
+      "localStorage",
+      "Events",
+      "conditional rendering",
+    ],
+  },
+  {
+    name: "ToDo App",
+    imageUrl: "./images/todo.jpg",
+    link: `https://todoapp.jacerchetoui.me`,
+    keywords: [
+      "useState",
+      "useEffect",
+      "useRef",
+      "form",
+      "localStorage",
+      "Events",
+      "conditional rendering",
+    ],
+  },
+  {
+    name: "Tenzies Game",
+    imageUrl: "./images/tenzies-game.jpg",
+    link: `https://tenzies-game.jacerchetoui.me`,
+    keywords: ["useState", "useEffect", "Events", "conditional rendering"],
+  },
+  {
+    name: "GitFinder",
+    imageUrl: "./images/gitfinder.jpg",
+    link: `https://githubfinder.jacerchetoui.me`,
+    keywords: ["tailwindCSS", "useState", "useEffect", "Events", "Fetch API"],
+  },
+  {
+    name: "Quizzical",
+    imageUrl: "./images/quizzical.jpg",
+    link: `https://quizzical.jacerchetoui.me`,
+    keywords: [
+      "useState",
+      "useEffect",
+      "useMemo",
+      "Events",
+      "conditional rendering",
+      "fetch API",
+    ],
+  },
+  {
+    name: "Shopping Cart",
+    imageUrl: "./images/cart.jpg",
+    link: `https://shopping-cart.jacerchetoui.me/`,
+    keywords: [
+      "SCSS",
+      "useReducer",
+      "useContext",
+      "useState",
+      "useEffect",
+      "Events",
+      "conditional rendering",
+    ],
+  },
+  {
+    name: "Stripe Submenu",
+    imageUrl: "./images/stripe.jpg",
+    link: `https://stripe-submenu.jacerchetoui.me`,
+    keywords: [
+      "useContext",
+      "useRef",
+      "useState",
+      "Events",
+      "conditional rendering",
+    ],
+  },
+  {
+    name: "SideBar",
+    imageUrl: "./images/sidebar.jpg",
+    link: `https://sidebar.jacerchetoui.me`,
+    keywords: [
+      "useContext",
+      "custom Hooks",
+      "useState",
+      "Events",
+      "conditional rendering",
+    ],
+  },
+  {
+    name: "Meme Generator",
+    imageUrl: "./images/memegenerator.jpg",
+    link: `https://memegenerator.jacerchetoui.me`,
+    keywords: [
+      "useState",
+      "useEffect",
+      "form",
+      "Events",
+      "conditional rendering",
+      "fetch API",
+    ],
+  },
+  {
+    name: "Slider",
+    imageUrl: "./images/slider.jpg",
+    link: `https://slider.jacerchetoui.me`,
+    keywords: ["useState", "useEffect", "Events", "conditional rendering"],
+  },
+  {
+    name: "Tours",
+    imageUrl: "./images/tours.jpg",
+    link: `https://tours.jacerchetoui.me`,
+    keywords: [
+      "useState",
+      "useEffect",
+      "fetch API",
+      "Events",
+      "conditional rendering",
+    ],
+  },
+  {
+    name: "Color Generator",
+    imageUrl: "./images/color-generator.jpg",
+    link: `https://colorgenerator.jacerchetoui.me`,
+    keywords: [
+      "form",
+      "useState",
+      "useEffect",
+      "fetch API",
+      "Events",
+      "conditional rendering",
+    ],
+  },
+  {
+    name: "Reviews",
+    imageUrl: "./images/reviews.jpg",
+    link: `https://reviews.jacerchetoui.me`,
+    keywords: ["useState", "Events"],
+  },
+  {
+    name: "navbar",
+    imageUrl: "./images/navbar.jpg",
+    link: `https://navbar.jacerchetoui.me`,
+    keywords: [
+      "useState",
+      "useEffect",
+      "useRef",
+      "Events",
+      "conditional rendering",
+    ],
+  },
+  {
+    name: "Food Menu",
+    imageUrl: "./images/food-menu.jpg",
+    link: `https://foodmenu.jacerchetoui.me`,
+    keywords: ["useState", "Events", "conditional rendering"],
+  },
+  {
+    name: "Lorem Generator",
+    imageUrl: "./images/lorem-generator.jpg",
+    link: `https://loremgenerator.jacerchetoui.me`,
+    keywords: ["useState", "conditional rendering", "form"],
+  },
+];
 
-export default projects
+export default projects;
